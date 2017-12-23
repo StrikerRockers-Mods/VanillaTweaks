f598c4bae635f265b3cfe63246b64fa8b8b9d2e0
StrikerRocker <strikerrockerplays@gmail.com>
2017-12-23 13:03:57 +0530

    Fixes Server crash

--- src/main/java/com/strikerrocker/vt/handlers/VTSoundHandler.java
+++ src/main/java/com/strikerrocker/vt/handlers/VTSoundHandler.java
@@ -3,10 +3,13 @@ package com.strikerrocker.vt.handlers;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class VTSoundHandler {
     // LOWEST Priority so that everything else can do their thing if they need to first
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = false)
+    @SideOnly(Side.CLIENT)
     public void onEvent(PlaySoundEvent event) {
 
 
