9dd047d704e8dc2d68a61db543511f1283c8ae01
StrikerRocker <thariksherief1@gmail.com>
2018-02-05 17:24:02 +0530

    Fixes #19 Closes #20

--- src/main/java/com/strikerrocker/vt/compat/baubles/BaubleTools.java
+++ src/main/java/com/strikerrocker/vt/compat/baubles/BaubleTools.java
@@ -3,22 +3,23 @@ package com.strikerrocker.vt.compat.baubles;
 import baubles.api.BaublesApi;
 import baubles.api.cap.IBaublesItemHandler;
 import com.strikerrocker.vt.items.VTItems;
-import com.strikerrocker.vt.vt;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Loader;
 
 public class BaubleTools {
     public static boolean hasProbeGoggle(EntityPlayer player) {
-        if (vt.baubles) {
+        if (Loader.isModLoaded("baubles")) {
             IBaublesItemHandler handler = BaublesApi.getBaublesHandler(player);
             if (handler == null) {
                 return false;
             }
             ItemStack stackInSlot = handler.getStackInSlot(4);
             return !stackInSlot.isEmpty() && stackInSlot.getItem() == VTItems.bb;
+        } else {
+            return false;
         }
-        return false;
     }
 
     public static Item initBinocularBauble() {
--- src/main/java/com/strikerrocker/vt/vtModInfo.java
+++ src/main/java/com/strikerrocker/vt/vtModInfo.java
@@ -17,7 +17,7 @@ public final class vtModInfo {
     /**
      * Vanilla Tweaks's version
      */
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.2.4";
 
     /**
      * Vanilla Tweaks's package location
