eee4ae892b1ac149692be02ae40c0e4795fba664
StrikerRocker <thariksherief1@gmail.com>
2018-04-15 12:19:44 +0530

    Closes #33

--- src/main/java/io/github/strikerrocker/vt/vtModInfo.java
+++ src/main/java/io/github/strikerrocker/vt/vtModInfo.java
@@ -18,7 +18,7 @@ public final class vtModInfo
     /**
      * Vanilla Tweaks's version
      */
-    public static final String VERSION = "1.2.9";
+    public static final String VERSION = "1.2.11";
 
     /**
      * Vanilla Tweaks's package location
