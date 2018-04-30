993d67b4a9cd30676551da99ab428430886112e5
StrikerRocker <strikerrockerplays@gmail.com>
2018-04-30 18:01:06 +0530

    Version Bump for curse beta release

--- src/main/java/io/github/strikerrocker/vt/VTModInfo.java
+++ src/main/java/io/github/strikerrocker/vt/VTModInfo.java
@@ -17,7 +17,7 @@ public final class VTModInfo {
     /**
      * Vanilla Tweaks's version
      */
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
 
     /**
      * Vanilla Tweaks's package location
