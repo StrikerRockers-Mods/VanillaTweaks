f8f85cec5c5ad530b309dba08eb73b166db05613
Striker Rocker <thariksherief1@gmail.com>
2018-07-03 21:02:02 +0530

    Added javadocs and removed an un-used Utils method

--- src/main/java/io/github/strikerrocker/vt/misc/VTUtils.java
+++ src/main/java/io/github/strikerrocker/vt/misc/VTUtils.java
@@ -22,7 +22,6 @@ import net.minecraftforge.oredict.DyeUtils;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -43,27 +42,6 @@ public class VTUtils {
     public static FakePlayer getFakePlayer(World world) {
         if (world instanceof WorldServer)
             return FakePlayerFactory.getMinecraft((WorldServer) world);
-        return null;
-    }
-
-    /**
-     * Finds an object by the specified name(s) in the specified object, and returns it
-     *
-     * @param fieldContainer The object to find the object in
-     * @param fieldNames     A list of all possible names for the object
-     * @param <T>            The data type of the object to return
-     * @return An object of the specified type with the first possible of the passed names
-     */
-    public static <T> T findObject(Object fieldContainer, String... fieldNames) {
-        Class fieldClass = fieldContainer.getClass();
-        while (fieldClass != null) {
-            try {
-                Field field = ReflectionHelper.findField(fieldContainer.getClass(), fieldNames);
-                return (T) field.get(fieldContainer);
-            } catch (Exception exception) {
-                fieldClass = fieldClass.getSuperclass();
-            }
-        }
         return null;
     }
 
