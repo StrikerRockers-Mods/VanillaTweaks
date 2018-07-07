6dd207a55d07c7a5d4c468c92d057d38495d998f
StrikerRocker <strikerrockerplays@gmail.com>
2018-07-07 20:05:47 +0530

    Made sure recipes for stairs werent registering multiple times and fixed some borked recipes from last refractor

--- src/main/java/io/github/strikerrocker/vt/VTModInfo.java
+++ src/main/java/io/github/strikerrocker/vt/VTModInfo.java
@@ -17,7 +17,7 @@ public final class VTModInfo {
     /**
      * Vanilla Tweaks's version
      */
-    public static final String VERSION = "1.3.4";
+    public static final String VERSION = "1.3.5";
 
     /**
      * Vanilla Tweaks's package location
