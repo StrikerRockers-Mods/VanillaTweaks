c88a9c594e32be12a3e2f819dfacaa588e4ae513
StrikerRocker <strikerrockerplays@gmail.com>
2018-08-21 19:51:28 +0530

    Bump version

--- src/main/java/io/github/strikerrocker/vt/VTModInfo.java
+++ src/main/java/io/github/strikerrocker/vt/VTModInfo.java
@@ -17,7 +17,7 @@ public final class VTModInfo {
     /**
      * Vanilla Tweaks's version
      */
-    public static final String VERSION = "1.3.5";
+    public static final String VERSION = "1.3.6";
 
     /**
      * Vanilla Tweaks's package location
