6f4a9d2e268654bb4854780703ec43de1c36c88b
StrikerRocker <strikerrockerplays@gmail.com>
2019-01-19 19:23:12 +0530

    Added version.json and kept version.java for compat and bump version

--- version.java
+++ version.java
@@ -1,10 +1,10 @@
 {
-        "homepage":"https://minecraft.curseforge.com/projects/292852",
-        "promos":{
-        "1.12.2-latest":"1.3.6"
+        "homepage": "https://minecraft.curseforge.com/projects/292852",
+        "promos": {
+        "1.12.2-latest": "1.4.5"
         },
-        "1.12.2":{
-        "1.3.6":"* Arrows which hit ground and are on fire sets the ground to fire\n* TNT placed adjacent to lava and magma block ignites itself"
-
+        "1.12.2": {
+            "1.3.6": "* Arrows which hit ground and are on fire sets the ground to fire\n* TNT placed adjacent to lava and magma block ignites itself",
+            "1.4.5": "* Fix fire arrows being able to replace blocks above on hit\n* Added support for enchantment description mod"
         }
-        }
+}
