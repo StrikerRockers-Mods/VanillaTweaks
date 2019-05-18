7242a7c6583c232f8c315a6b71800da381da4724
StrikerRocker <strikerrockerplays@gmail.com>
2019-05-18 19:54:08 +0530

    -Fixed compatability with other's mods such as iron chest's and zawa (They should really only initialize entities only when they use them).

--- src/main/java/io/github/strikerrocker/vt/capabilities/CapabilitySelfPlanting.java
+++ src/main/java/io/github/strikerrocker/vt/capabilities/CapabilitySelfPlanting.java
@@ -1,11 +1,18 @@
 package io.github.strikerrocker.vt.capabilities;
 
+import io.github.strikerrocker.vt.VTModInfo;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
 import net.minecraftforge.common.capabilities.CapabilityManager;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class CapabilitySelfPlanting {
     @CapabilityInject(ISelfPlanting.class)
@@ -28,5 +35,18 @@ public class CapabilitySelfPlanting {
                 instance.setSteadyTicks(compound.getInteger(SelfPlantingHandler.STEADY_TICKS_KEY));
             }
         }, SelfPlantingHandler::new);
+        MinecraftForge.EVENT_BUS.register(CapabilitySelfPlanting.class);
+    }
+
+    /**
+     * Attach the self planting capability
+     *
+     * @param event AttachCapabilityEvent
+     */
+    @SubscribeEvent
+    public static void attachCapabilityEntity(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof EntityItem) {
+            event.addCapability(new ResourceLocation(VTModInfo.MODID, "planting"), new SelfPlantingProvider());
+        }
     }
 }
