0c2ee91b0d9116e195c1f64cea6154ec406a7d1c
StrikerRocker <strikerrockerplays@gmail.com>
2019-07-11 19:44:52 +0530

    Fixed NoMoreLavaGen not working. Closes #58

--- src/main/java/io/github/strikerrocker/vt/world/NoMoreLavaGen.java
+++ src/main/java/io/github/strikerrocker/vt/world/NoMoreLavaGen.java
@@ -1,6 +1,7 @@
 package io.github.strikerrocker.vt.world;
 
 import io.github.strikerrocker.vt.base.Feature;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.event.terraingen.PopulateChunkEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
@@ -12,19 +13,17 @@ import static net.minecraftforge.event.terraingen.PopulateChunkEvent.Populate.Ev
 public class NoMoreLavaGen extends Feature {
     private boolean noMoreLavaPocketGen;
 
+    public NoMoreLavaGen() {
+        MinecraftForge.TERRAIN_GEN_BUS.register(this);
+    }
+
     @Override
     public void syncConfig(Configuration config, String category) {
         noMoreLavaPocketGen = config.get(category, "noMoreLavaPocketGen", true, "Annoyed of the pesky lava pockets in nether").getBoolean();
     }
 
-    @Override
-    public boolean usesEvents() {
-        return true;
-    }
-
     @SubscribeEvent
     public void onPopulateChunk(PopulateChunkEvent.Populate event) {
-        //TODO Fix this
         if (noMoreLavaPocketGen) {
             if (event.getType() == NETHER_LAVA || event.getType() == NETHER_LAVA2) {
                 event.setResult(Event.Result.DENY);
