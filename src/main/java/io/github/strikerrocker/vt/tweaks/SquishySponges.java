86f4f26d7f3b07b598c2bf8c3694d56782c481d4
Tharik Sherief <thariksherief1@gmail.com>
2019-09-14 20:40:21 +0530

    Fixed veteran enchantment. Added SquishySponges feature.

---
+++ src/main/java/io/github/strikerrocker/vt/tweaks/SquishySponges.java
@@ -0,0 +1,51 @@
+package io.github.strikerrocker.vt.tweaks;
+
+import io.github.strikerrocker.vt.base.Feature;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.WetSpongeBlock;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class SquishySponges extends Feature {
+    private ForgeConfigSpec.BooleanValue squishySponge;
+
+    private static void turnIntoWater(World worldIn, BlockPos pos) {
+        if (worldIn.dimension.doesWaterVaporize()) {
+            worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
+        } else {
+            worldIn.setBlockState(pos, Fluids.WATER.getDefaultState().getBlockState(), 11);
+        }
+    }
+
+    @Override
+    public boolean usesEvents() {
+        return true;
+    }
+
+    @Override
+    public void setupConfig(ForgeConfigSpec.Builder builder) {
+        squishySponge = builder
+                .translation("config.vanillatweaks:squishySponge")
+                .comment("Do you want to neglect fall damage and get water spilled when you fall on a wet sponge?")
+                .define("squishySponge", true);
+    }
+
+    @SubscribeEvent
+    public void onLivingFall(LivingFallEvent event) {
+        World world = event.getEntity().getEntityWorld();
+        LivingEntity entity = event.getEntityLiving();
+        BlockPos pos = entity.getPosition().down();
+        if (entity instanceof PlayerEntity && squishySponge.get() && world.getBlockState(pos).getBlock() instanceof WetSpongeBlock) {
+            turnIntoWater(world, pos.offset(Direction.Plane.HORIZONTAL.random(world.rand)));
+            world.setBlockState(pos, Blocks.SPONGE.getDefaultState());
+            event.setCanceled(true);
+        }
+    }
+}
