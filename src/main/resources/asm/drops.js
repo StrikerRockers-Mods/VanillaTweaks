8a4cf9489f363abd50c778bb37e9c60e1e9cee61
strikerrocker <strikerrockerplays@gmail.com>
2019-12-24 19:03:31 +0530

    Temporary fix for #80

--- src/main/resources/asm/drops.js
+++ src/main/resources/asm/drops.js
@@ -8,7 +8,7 @@ function initializeCoreMod() {
                 'methodDesc': '(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/storage/loot/LootContext$Builder;)Ljava/util/List;'
             },
             'transformer': function(method) {
-              print('[UnstableTools]: Patching Minecraft\' Block#getDrops');
+              print('[VanillaTweaks]: Patching Minecraft\' Block#getDrops');
 
                 var ASM = Java.type('net.minecraftforge.coremod.api.ASMAPI');
                 var Opcodes = Java.type('org.objectweb.asm.Opcodes');
