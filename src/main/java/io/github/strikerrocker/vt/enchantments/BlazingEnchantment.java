f8f8867b25f89d0e31ab4f9fe09197e03ceccad3
strikerrocker <strikerrockerplays@gmail.com>
2020-04-12 19:23:24 +0530

    Removed ASM and implemented global loot function for siphon and blazing enchantment

--- src/main/java/io/github/strikerrocker/vt/enchantments/BlazingEnchantment.java
+++ src/main/java/io/github/strikerrocker/vt/enchantments/BlazingEnchantment.java
@@ -1,53 +1,30 @@
 package io.github.strikerrocker.vt.enchantments;
 
+import com.google.gson.JsonObject;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentType;
 import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolItem;
 import net.minecraft.item.crafting.FurnaceRecipe;
 import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.storage.loot.conditions.ILootCondition;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.items.ItemHandlerHelper;
 
+import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.SplittableRandom;
 
 public class BlazingEnchantment extends Enchantment {
     BlazingEnchantment(String name) {
         super(Rarity.VERY_RARE, EnchantmentType.DIGGER, new EquipmentSlotType[]{EquipmentSlotType.MAINHAND});
         this.setRegistryName(name);
-    }
-
-    public static void harvestDropEvent(List<ItemStack> drops, PlayerEntity player, ItemStack tool) {
-        if (EnchantmentFeature.enableBlazing.get()) {
-            List<ItemStack> dropsCopy = new ArrayList<>(drops);
-            drops.clear();
-            int fortuneLvl = EnchantmentHelper.getEnchantmentLevel(Enchantments.FORTUNE, tool);
-            for (ItemStack drop : dropsCopy) {
-                if (!drop.isEmpty()) {
-                    Optional<FurnaceRecipe> optional = player.world.getRecipeManager().getRecipe(IRecipeType.SMELTING, new Inventory(drop), player.world);
-                    if (optional.isPresent()) {
-                        ItemStack smeltingResult = optional.get().getRecipeOutput();
-                        if (!smeltingResult.isEmpty()) {
-                            smeltingResult = smeltingResult.copy();
-                            smeltingResult.setCount(smeltingResult.getCount() * drop.getCount());
-                            if (!(smeltingResult.getItem() instanceof BlockItem))
-                                smeltingResult.setCount(smeltingResult.getCount() * (new SplittableRandom().nextInt(fortuneLvl + 1) + 1));
-                            drops.add(smeltingResult);
-                        } else
-                            drops.add(drop);
-                    } else
-                        drops.add(drop);
-                }
-            }
-        }
-        //TODO remove asm when https://github.com/MinecraftForge/MinecraftForge/pull/5871 is merged
     }
 
     @Override
@@ -78,5 +55,35 @@ public class BlazingEnchantment extends Enchantment {
     @Override
     public boolean isTreasureEnchantment() {
         return EnchantmentFeature.enableBlazing.get();
+    }
+
+    private static class BlazingModifier extends LootModifier {
+        public BlazingModifier(ILootCondition[] conditionsIn) {
+            super(conditionsIn);
+        }
+
+        private static ItemStack smelt(ItemStack stack, LootContext context) {
+            System.out.println("called blazing");
+            return context.getWorld().getRecipeManager().getRecipe(IRecipeType.SMELTING, new Inventory(stack), context.getWorld())
+                    .map(FurnaceRecipe::getRecipeOutput)
+                    .filter(itemStack -> !itemStack.isEmpty())
+                    .map(itemStack -> ItemHandlerHelper.copyStackWithSize(itemStack, stack.getCount() * itemStack.getCount()))
+                    .orElse(stack);
+        }
+
+        @Nonnull
+        @Override
+        public List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+            ArrayList<ItemStack> ret = new ArrayList<>();
+            generatedLoot.forEach((stack) -> ret.add(smelt(stack, context)));
+            return ret;
+        }
+    }
+
+    public static class Serializer extends GlobalLootModifierSerializer<BlazingModifier> {
+        @Override
+        public BlazingModifier read(ResourceLocation name, JsonObject json, ILootCondition[] conditionsIn) {
+            return new BlazingModifier(conditionsIn);
+        }
     }
 }
