5874e8cdf46ccb850ca33294b229de14840564a5
StrikerRocker <strikerrockerplays@gmail.com>
2020-05-13 16:04:41 +0530

    Re-implemented self planting.

--- src/main/java/io/github/strikerrocker/vt/world/selfplanting/SelfPlanting.java
+++ src/main/java/io/github/strikerrocker/vt/world/selfplanting/SelfPlanting.java
@@ -1,11 +1,13 @@
 package io.github.strikerrocker.vt.world.selfplanting;
 
+import io.github.strikerrocker.vt.base.Feature;
 import io.github.strikerrocker.vt.misc.Utils;
-import io.github.strikerrocker.vt.world.WorldModule;
 import net.minecraft.block.Block;
+import net.minecraft.block.FlowerBlock;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
@@ -13,54 +15,82 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.util.FakePlayer;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.item.ItemExpireEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-import java.util.Random;
+public class SelfPlanting extends Feature {
 
-public class SelfPlanting implements ISelfPlanting {
-    static final String MIN_STEADY_TICKS_KEY = "MinSteadyTicks";
-    static final String STEADY_TICKS_KEY = "SteadyTicks";
-    private static Random random = new Random();
-    private int minSteadyTicks = 0;
-    private int steadyTicks = 0;
+    private ForgeConfigSpec.IntValue despawnTime;
+    private ForgeConfigSpec.IntValue chanceToPlant;
+    private ForgeConfigSpec.BooleanValue selfPlanting;
 
     @Override
-    public int getMinSteadyTicks() {
-        return minSteadyTicks;
+    public boolean usesEvents() {
+        return true;
     }
 
     @Override
-    public void setMinSteadyTicks(int minSteadyTicks) {
-        this.minSteadyTicks = minSteadyTicks;
+    public void setupConfig(ForgeConfigSpec.Builder builder) {
+        selfPlanting = builder
+                .translation("config.vanillatweaks:selfPlanting")
+                .comment("Want seeds to auto-plant themselves when broken?")
+                .define("selfPlanting", true);
+        despawnTime = builder
+                .translation("config.vanillatweaks:despawnTime")
+                .comment("How long a plant should take to despawn (and attempt to plant) Default Minecraft is 6000.")
+                .defineInRange("despawnTime", 1000, 0, Integer.MAX_VALUE);
+        chanceToPlant = builder
+                .translation("config.vanillatweaks:chanceToPlant")
+                .comment("Percentage chance to plant")
+                .defineInRange("chanceToPlant", 100, 0, 100);
     }
 
-    @Override
-    public int getSteadyTicks() {
-        return steadyTicks;
+    @SubscribeEvent
+    public void itemDecay(ItemExpireEvent event) {
+        plant(event.getEntityItem(), event);
     }
 
-    @Override
-    public void setSteadyTicks(int steadyTicks) {
-        this.steadyTicks = steadyTicks;
-    }
-
-    @Override
-    public void handlePlantingLogic(ItemEntity entity) {
-        Item item = entity.getItem().getItem();
-        if (item instanceof BlockItem && Block.getBlockFromItem(item) instanceof IPlantable) {
-            if (this.minSteadyTicks == 0)
-                this.minSteadyTicks = random.nextInt(75) + WorldModule.selfPlanting.minTicks.get();
-            this.steadyTicks++;
-            BlockPos entityPos = new BlockPos(entity);
-            Vec3d entityVec = new Vec3d(entityPos.getX(), entityPos.getY(), entityPos.getZ());
-            BlockPos lastTickEntityPos = new BlockPos(entity.lastTickPosX, entity.lastTickPosY, entity.lastTickPosZ);
-            if (entityPos.compareTo(lastTickEntityPos) != 0)
-                this.steadyTicks = 0;
-            BlockRayTraceResult rayTraceResult = entity.world.rayTraceBlocks(
-                    new RayTraceContext(entityVec.add(0, 1, 0), entityVec, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, entity));
-            if (this.steadyTicks >= this.minSteadyTicks && entity.getItem().getItem().onItemUse(new ItemUseContext(Utils.getFakePlayer(entity.world), Hand.MAIN_HAND, rayTraceResult)) == ActionResultType.SUCCESS) {
-                entity.getItem().shrink(1);
+    @SubscribeEvent
+    public void itemToss(EntityJoinWorldEvent event) {
+        if (!event.getWorld().isRemote && event.getEntity() instanceof ItemEntity) {
+            ItemEntity itemEntity = (ItemEntity) event.getEntity();
+            Item item = itemEntity.getItem().getItem();
+            if (selfPlanting.get() && item instanceof BlockItem && !(Block.getBlockFromItem(item) instanceof FlowerBlock)) {
+                itemEntity.lifespan = despawnTime.get();
             }
         }
+    }
+
+
+    public void plant(ItemEntity entity, ItemExpireEvent event) {
+        World world = entity.getEntityWorld();
+        ItemStack stack = entity.getItem().copy();
+        Item item = stack.getItem();
+        Block block = Block.getBlockFromItem(item);
+        BlockPos entityPos = new BlockPos(entity);
+        if (selfPlanting.get() && item instanceof BlockItem && block instanceof IPlantable && !(block instanceof FlowerBlock) && world.rand.nextInt() > chanceToPlant.get()) {
+            FakePlayer player = Utils.getFakePlayer(world);
+            Vec3d entityVec = new Vec3d(entityPos.getX(), entityPos.getY(), entityPos.getZ());
+            System.out.println(stack);
+            BlockRayTraceResult rayTraceResult = entity.world.rayTraceBlocks(
+                    new RayTraceContext(entityVec.add(0, 2, 0), entityVec.add(0, -1, 0), RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, entity));
+            ActionResultType result = item.onItemUse(new ItemUseContext(player, Hand.MAIN_HAND, rayTraceResult));
+            System.out.println(result);
+            System.out.println(stack);
+            if (result == ActionResultType.SUCCESS) {
+                if (stack.getCount() > 0) {
+                    stack.shrink(1);
+                }
+            }
+            System.out.println(stack);
+            if (stack.getCount() > 0)
+                world.addEntity(new ItemEntity(world, entityPos.getX(), entityPos.getY() + 1, entityPos.getZ(), stack));
+        } else
+            world.addEntity(new ItemEntity(world, entityPos.getX(), entityPos.getY() + 1, entityPos.getZ(), stack));
     }
 }
